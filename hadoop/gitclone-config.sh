38a677aa514aec70c57e63f6f3ab1c98fcb83b77
Song <soog2008@hotmail.com>
2018-09-11 16:56:04 +0800

    Create gitclone-config.sh

---
+++ hadoop/gitclone-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ "${GIT_URL}" -a "${SUB_FOLDER}" ];then
+    echo "Git ENVs are exists."
+else
+    echo "You need define ENV GIT_URL and SUB_FOLDER"
+    exit 1
+fi
+
+git init /hdfs-config
+cd /hdfs-config 
+git remote add origin ${GIT_URL}
+git config core.sparsecheckout true 
+echo ${SUB_FOLDER} >> .git/info/sparse-checkout
+git pull origin master
+cd ${SUB_FOLDER}
+mv tool /tool
+mv -f config/* /usr/local/hadoop/etc/hadoop
+mv supervisord /supervisord
+rm -rf /hdfs-config
